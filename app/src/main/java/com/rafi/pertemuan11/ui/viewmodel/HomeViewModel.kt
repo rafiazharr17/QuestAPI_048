484c3c3fbd58c4d6547547d9d1ff8f4861a9d2d9
rafiazharr17 <rafipilo17@gmail.com>
2024-12-29 06:39:49 +0700

    membuat variabel mhsUiState dan fungsi getMhs untuk mengambil data mahasiswa

--- app/src/main/java/com/rafi/pertemuan11/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/rafi/pertemuan11/ui/viewmodel/HomeViewModel.kt
@@ -1,8 +1,15 @@
 package com.rafi.pertemuan11.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.rafi.pertemuan11.model.Mahasiswa
 import com.rafi.pertemuan11.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed class HomeuiState{
     data class Success(
@@ -15,5 +22,19 @@ sealed class HomeuiState{
 class HomeViewModel(
     private val mhs: MahasiswaRepository
 ): ViewModel(){
+    var mhsUiState: HomeuiState by mutableStateOf(HomeuiState.Loading)
+        private set
 
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeuiState.Loading
+            mhsUiState = try {
+                HomeuiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException){
+                HomeuiState.Error
+            } catch (e: HttpException){
+                HomeuiState.Error
+            }
+        }
+    }
 }
