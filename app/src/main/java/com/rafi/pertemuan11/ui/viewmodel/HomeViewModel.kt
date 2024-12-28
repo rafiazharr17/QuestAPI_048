36e604d12dea08664f1a533bce09bec847efdf90
rafiazharr17 <rafipilo17@gmail.com>
2024-12-29 06:36:20 +0700

    membuat class HomeViewModel untuk menampilkan data mahasiswa dari MahasiswaRepository

--- app/src/main/java/com/rafi/pertemuan11/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/rafi/pertemuan11/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,8 @@
 package com.rafi.pertemuan11.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.rafi.pertemuan11.model.Mahasiswa
+import com.rafi.pertemuan11.repository.MahasiswaRepository
 
 sealed class HomeuiState{
     data class Success(
@@ -8,4 +10,10 @@ sealed class HomeuiState{
     ): HomeuiState()
     object Error: HomeuiState()
     object Loading: HomeuiState()
+}
+
+class HomeViewModel(
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+
 }
