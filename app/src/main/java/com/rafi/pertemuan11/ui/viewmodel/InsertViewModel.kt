3a27254e6b96b050978f2cf2af47a31269531ac8
rafiazharr17 <rafipilo17@gmail.com>
2024-12-29 05:20:18 +0700

    membuat data class InsertUiEvent

---
+++ app/src/main/java/com/rafi/pertemuan11/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.rafi.pertemuan11.ui.viewmodel
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
