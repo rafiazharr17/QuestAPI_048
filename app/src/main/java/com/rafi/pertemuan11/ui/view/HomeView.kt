ef28202b90789a0e4a7db6e44246550eb8e4b31d
rafiazharr17 <rafipilo17@gmail.com>
2024-12-29 07:40:02 +0700

    membuat objek DestinasiHome yang merupakan implementasi dari DestinasiNavigasi

---
+++ app/src/main/java/com/rafi/pertemuan11/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.rafi.pertemuan11.ui.view
+
+import com.rafi.pertemuan11.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
