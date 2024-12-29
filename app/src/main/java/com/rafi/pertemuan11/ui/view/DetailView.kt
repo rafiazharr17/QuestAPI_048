546d1807680a06ab46a792a3501c6178fdf0b15f
rafiazharr17 <rafipilo17@gmail.com>
2024-12-30 05:02:05 +0700

    membuat fungsi ComponentDetailMhs dengan parameter judul dan isinya dibuat secara kolom

--- app/src/main/java/com/rafi/pertemuan11/ui/view/DetailView.kt
+++ app/src/main/java/com/rafi/pertemuan11/ui/view/DetailView.kt
@@ -1,5 +1,14 @@
 package com.rafi.pertemuan11.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.rafi.pertemuan11.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail : DestinasiNavigasi {
@@ -7,4 +16,28 @@ object DestinasiDetail : DestinasiNavigasi {
     override val titleRes = "Detail Mhs"
     const val NIM = "nim"
     val routeWithArg = "$route/{$NIM}"
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
