8720644dba47f4cf36dad8259befbfb85580d4a9
rafiazharr17 <rafipilo17@gmail.com>
2024-12-29 07:28:13 +0700

    membuat PenyediaViewModel untuk inisialisasi InsertViewModel dan HomeViewModel

---
+++ app/src/main/java/com/rafi/pertemuan11/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,27 @@
+package com.rafi.pertemuan11.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.rafi.pertemuan11.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            InsertViewModel(
+                aplikasiKontak().container.kontakRepository
+            )
+        }
+
+        initializer {
+            HomeViewModel(
+                aplikasiKontak().container.kontakRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
