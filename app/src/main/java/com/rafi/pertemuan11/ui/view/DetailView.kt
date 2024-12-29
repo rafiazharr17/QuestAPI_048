9d30de97cd8bb844a455764ee950111ceb3b9ad7
rafiazharr17 <rafipilo17@gmail.com>
2024-12-30 05:03:34 +0700

    membuat fungsi ItemDetailMhs dengan card yang berisi ComponentDetailMhs

--- app/src/main/java/com/rafi/pertemuan11/ui/view/DetailView.kt
+++ app/src/main/java/com/rafi/pertemuan11/ui/view/DetailView.kt
@@ -2,13 +2,19 @@ package com.rafi.pertemuan11.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.rafi.pertemuan11.model.Mahasiswa
 import com.rafi.pertemuan11.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail : DestinasiNavigasi {
@@ -16,6 +22,29 @@ object DestinasiDetail : DestinasiNavigasi {
     override val titleRes = "Detail Mhs"
     const val NIM = "nim"
     val routeWithArg = "$route/{$NIM}"
+}
+
+@Composable
+fun ItemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+) {
+    Card (
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+            ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+            ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
 }
 
 @Composable
