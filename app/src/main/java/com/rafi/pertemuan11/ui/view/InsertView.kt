ad9c623c61e9fe426fd14d5896134e48446fbfe7
rafiazharr17 <rafipilo17@gmail.com>
2024-12-29 07:04:05 +0700

    membuat objek DestinasiEntry yang merupakan implementasi dari DestinasiNavigasi

---
+++ app/src/main/java/com/rafi/pertemuan11/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.rafi.pertemuan11.ui.view
+
+import com.rafi.pertemuan11.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry_Mhs"
+}
